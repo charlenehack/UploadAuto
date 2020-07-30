6dba3be75f4665e92333a6a9783d76e07d4a74e9
charlenehack <37741776+charlenehack@users.noreply.github.com>
2020-07-30 10:41:03 +0800

    Update 1.go

--- 1.go
+++ 1.go
@@ -1,32 +1,47 @@
-/* ========== 字符串处理常用函数 ========== */
+/* ======== 时间和日期相关函数 ========== */
 func main() {
-	str := "hello, 我爱北京天安门"
-	fmt.Printf("字符串长度为：%d\n", len(str))   // 中文占3个字节
-	str1 := []rune(str)   // 将字符串转为切片类型
-	fmt.Printf("字符串类型：%T\n", str1)
-	n, err := strconv.Atoi("123")  // 字符串转整型，转换不成功时err接收错误
-	str = strconv.Itoa(12345)  // 整数转字符串
-	bytes := []byte("hello")   // 字符串转[]byte
-	str = string([]byte{97, 98, 99})  // byte转字符串
-	str = strconv.FormatInt(123, 2)  // 十进制转2，8，16进制，第一个参数十进制，第二个参数要转成的进制
-	strings.Contains("hello,world", "llo")  // 查找子串是否在指定的字符串中，存在返回true，不存在返回false
-	strings.Count("hello,world", "l")  // 统计字符串中有几个指定的子串
-	strings.EqualFold("abc", "ABC")  // 不区分大小写比较字符串，返回true / false；（ ==是区分大小写的）
-	strings.Index("comepro.com", "com")  // 返回子串在字符串第一次出现的index值，没有没有返回-1；也可用来查找字符串中是否存在子串
-	strings.LastIndex("comepro.com", "com") // 返回子串在字符串中最后一次出现的位置，没有返回-1
-	strings.Replace("hello,world", "h", "H", 1)  // 将字符串中指定子串替换成另一子串，n 指定替换几个，-1全部替换
-	strings.Split("hello,world", ",")  // 按指定字符将字符串拆分成字符串数组
-	strings.ToLower("GO")  // 大写转小写
-	strings.ToUpper("go")  // 小写转大写
-	strings.TrimSpace("  ABC  ")  // 去除字符串左右两边的空格，TrimSpaceLeft 去左 / TrimSpaceRight 去右
-	strings.Trim("hello!" , "!")  // 去除字符串左右两边的指定字符
-	strings.HasPrefix("Hello", "H")  // 判断字符串是否以指定字符串开头
-	strings.HasSuffix("hello", "o")  // 判断字符串是否以指定字符串结尾
-	fmt.Println(n, err, str, bytes)
-	// 字符串拼接效率最高方式
-	var build strings.Builder
-	build.WriteString("aaaaaaa")
-	build.WriteString("bbbbbbb")
-	ab := build.String()
-	fmt.Println(ab)
+	now := time.Now()  // 获取当前时间
+	fmt.Println("年：", now.Year())
+	fmt.Println("月：", now.Month())  // int(now.Month())可将月转为数字形式
+	fmt.Println("日：", now.Day())
+	fmt.Println("时：", now.Hour())
+	fmt.Println("分：", now.Minute())
+	fmt.Println("秒：", now.Second())
+	// 格式化日期时间赋值给变量
+	dateStr := fmt.Sprintf("当前时间：%d-%d-%d %d:%d:%d\n", now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), now.Second())
+	fmt.Println(dateStr)
+	fmt.Printf(now.Format("2006-01-02 15:04:05"))  // 格式化日期时间的另一种方式，分隔符可自定义，各个数字是固定的不能改
+	fmt.Printf(now.Format("2006-01-02"))  // 可以根据数字按需取出想要的字段，如"01"只取出月份
+	fmt.Printf(now.Format("15:04:05"))
+	time.Sleep(time.Second)   // 休眠一秒
+	time.Sleep(time.Millisecond * 100)  // 休眠一毫秒
+	fmt.Printf("Unix时间戳：%v，Unixnano时间戳：%v", now.Unix(), now.UnixNano())
+	// 时间加减
+	m, _ := time.ParseDuration("-1m")  	// 10分钟前
+	m1 := now.Add(m)
+
+	h, _ := time.ParseDuration("-1h")  	// 8个小时前
+	h1 := now.Add(8 * h)
+
+	d, _ := time.ParseDuration("-24h")   // 一天前
+	d1 := now.Add(d)
+
+
+	mm, _ := time.ParseDuration("1m")    // 10分钟后
+	mm1 := now.Add(mm)
+
+	hh, _ := time.ParseDuration("1h")    // 8小时后
+	hh1 := now.Add(hh)
+
+	dd, _ := time.ParseDuration("24h")  	// 一天后
+	dd1 := now.Add(dd)
+
+	subM := now.Sub(m1)  	// Sub 计算两个时间差
+	fmt.Println(subM.Minutes(), "分钟")
+
+	sumH := now.Sub(h1)
+	fmt.Println(sumH.Hours(), "小时")
+
+	sumD := now.Sub(d1)
+	fmt.Printf("%v 天\n", sumD.Hours()/24)
 }
