62bbbff6798f372351929e1f2b1a4edb26787122
charlenehack <37741776+charlenehack@users.noreply.github.com>
2020-07-30 09:53:45 +0800

    Update 1.go

--- 1.go
+++ 1.go
@@ -1,46 +1,58 @@
-/* ========== 循环 ========== */
+/* ========== 函数、自定义数据类型 ========== */
+func getSum(n1 int, n2 int) (sum int) {  // 直接为函数返回值命名，return后没必须再接返回值，好处是多个返回值时不用关心返回值位置顺序
+	sum = n1 + n2
+	return
+}
+
+func sum(n1 int, args... int) int {  // 函数可变参数传参，args关键字可自定义
+	sum := n1
+	for i := 0; i < len(args); i++ {
+		sum += args[i]
+	}
+	return sum
+}
+
+type myint int  // 给int类型取别名，此时虽然myint和int都是int类型，但go依然认为是两个类型
+type myFuncType func(int, int) int  // 自定义函数数据类型，myFuncType为传入两个int类型，返回一个int类型的函数
+
+func myFun(funvar myFuncType, num1 int, num2 int) int {   // 调用自定义函数类型，funvar为声明函数变量
+	return funvar(num1, num2)
+}
+
+func init() {  // init函数主要用来完成一些初始化的工作；执行流程：全局变量定义-->init函数-->main函数
+	fmt.Println("我是init函数。")
+}
+
+func Sum(n1 int, n2 int) int {
+	defer fmt.Println("我第3个执行！")   // 当执行到defer时，暂时不执行，先将defer后面的语句压入到独立栈中
+	defer fmt.Println("我第2个执行！")   // 当函数执行完毕后，再从栈中按照先入后出的方式出栈，执行
+
+	res := n1 + n2
+	fmt.Println("我第1个执行！")
+	return res
+}
+var (add = func(n1 int, n2 int) int {   // 定义一个全局匿名函数并赋值给一个变量
+	return n1 + n2
+})
+
 func main() {
-	i := 0
-	for ; i <= 10; i++ {
-		fmt.Println("hello", i)
-	}
+	res := myFun(getSum, 20, 30)
+	println(res)
+	res1 := sum(1, 3,5,8,9)
+	println(res1)
 
-	j := 1
-	for j <= 10 {
-		fmt.Println("hello", j)
-		j++
-	}
+	res3 := func(n1 int, n2 int) int {   // 匿名函数的使用
+		return n1 + n2
+	}(10, 20)  // 传参
+	println(res3)
 
-	for { // 等价于 for ; ; {
-		fmt.Println("我是死循环，请用break帮我退出")
-		break
+	a := func(n1 int, n2 int) int {   // 将匿名函数赋值给一个变量
+		return n1 - n2
 	}
+	res4 := a(30, 20)  // 通过变量名调用匿名函数
+	fmt.Printf("a type is %T\n", a)   // 此时a为一个函数数据类型
+	println(res4)
 
-	var str string = "hello, world!"
-	//	str1 = []rune(str)  // 如果字符串中存在中文会乱码，需先转为切片类型
-	for i := 0; i < len(str); i++ { // 字符串遍历一
-		fmt.Printf("%c\n", str[i])
-	}
-	for index, val := range str { // 字符串遍历二，中文不会乱码，一个汉字占3字节
-		fmt.Printf("index = %d, val = %c", index, val)
-	}
-
-	var count int = 0
-	rand.Seed(time.Now().UnixNano()) // 生成随机数需要给rand设置一个种子，UnixNano表示刷新频率为纳秒级，可根据需求设置频率
-	for {
-		a := rand.Intn(100) // 生成随机数
-		count++
-		fmt.Printf("生成的随机数为：%d, 生成次数：%d\n", a, count)
-		if a == 50 {
-			break // break默认中止最近的循环，可给循环打上标签，通过break指定标签中止标签对应的循环
-		}
-	}
-
-	println("我是label1")
-	if count > 0 {
-		goto label3 // 跳到指定标签处执行
-	}
-	println("我是label2")
-	label3:
-	println("我是label3")
+	res5 := add(30, 20)   // 调用全局匿名函数
+	println(res5)
 }
