2af68fb147c1aafaa50ff986efaac26589bf5996
charlenehack <37741776+charlenehack@users.noreply.github.com>
2020-07-30 10:06:53 +0800

    Update 1.go

--- 1.go
+++ 1.go
@@ -1,58 +1,27 @@
-/* ========== 函数、自定义数据类型 ========== */
-func getSum(n1 int, n2 int) (sum int) {  // 直接为函数返回值命名，return后没必须再接返回值，好处是多个返回值时不用关心返回值位置顺序
-	sum = n1 + n2
-	return
-}
-
-func sum(n1 int, args... int) int {  // 函数可变参数传参，args关键字可自定义
-	sum := n1
-	for i := 0; i < len(args); i++ {
-		sum += args[i]
+/* ========== 闭包 ========== */
+// 闭包是一个函数和与其相关的引用环境组合的一个整体
+func AddUpper() func(int) int {  // 定义一个AddUpper函数，返回的数据类型是func(int) int
+	n := 10
+	return func(x int) int {  // 返回一个匿名函数，但这个匿名函数引用到函数外的n，因此这个匿名函数就和n形成一个整体，构成闭包
+		n = n + x
+		return n
 	}
-	return sum
 }
 
-type myint int  // 给int类型取别名，此时虽然myint和int都是int类型，但go依然认为是两个类型
-type myFuncType func(int, int) int  // 自定义函数数据类型，myFuncType为传入两个int类型，返回一个int类型的函数
-
-func myFun(funvar myFuncType, num1 int, num2 int) int {   // 调用自定义函数类型，funvar为声明函数变量
-	return funvar(num1, num2)
+func makeSuffix(suffix string) func(string) string {  // 闭包使用实例，传入一个文件名，如果是指定后缀则返回文件名，否则加上指定后缀
+	return func(name string) string {   // 返回的匿名函数与外部变量suffix形成一个闭包，因为返回的函数引用到suffix这个变量
+		if !strings.HasSuffix(name, suffix) {   // HasSuffix方法判断字符串是否以..结尾
+			return name + suffix
+		}
+		return name
+	}
 }
-
-func init() {  // init函数主要用来完成一些初始化的工作；执行流程：全局变量定义-->init函数-->main函数
-	fmt.Println("我是init函数。")
-}
-
-func Sum(n1 int, n2 int) int {
-	defer fmt.Println("我第3个执行！")   // 当执行到defer时，暂时不执行，先将defer后面的语句压入到独立栈中
-	defer fmt.Println("我第2个执行！")   // 当函数执行完毕后，再从栈中按照先入后出的方式出栈，执行
-
-	res := n1 + n2
-	fmt.Println("我第1个执行！")
-	return res
-}
-var (add = func(n1 int, n2 int) int {   // 定义一个全局匿名函数并赋值给一个变量
-	return n1 + n2
-})
 
 func main() {
-	res := myFun(getSum, 20, 30)
-	println(res)
-	res1 := sum(1, 3,5,8,9)
-	println(res1)
-
-	res3 := func(n1 int, n2 int) int {   // 匿名函数的使用
-		return n1 + n2
-	}(10, 20)  // 传参
-	println(res3)
-
-	a := func(n1 int, n2 int) int {   // 将匿名函数赋值给一个变量
-		return n1 - n2
-	}
-	res4 := a(30, 20)  // 通过变量名调用匿名函数
-	fmt.Printf("a type is %T\n", a)   // 此时a为一个函数数据类型
-	println(res4)
-
-	res5 := add(30, 20)   // 调用全局匿名函数
-	println(res5)
+	f := AddUpper()
+	fmt.Println(f(2))
+	fmt.Println(f(5))  // 当反复调用f函数时，因为n是初始化一次，因此每调用一次值就会改变而不是重新初始化
+	fmt.Println(f(7))
+	f1 := makeSuffix(".jpg")
+	fmt.Println(f1("aaa"))
 }
