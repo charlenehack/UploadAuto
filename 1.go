9522c4f9225c72679a0af55882987085fff3388b
charlenehack <37741776+charlenehack@users.noreply.github.com>
2020-07-30 09:26:15 +0800

    Update 1.go

--- 1.go
+++ 1.go
@@ -1,17 +1,20 @@
-/* ========== 指针 ========== */
+
+/* ========== switch..case ========== */
 func main() {
-	i := 10
-	fmt.Printf("i的值为%d, i的内存地址为：%v\n", i, &i)
+	var key, c byte
+	fmt.Println("请输入一个字符 a, b, c, d")
+	fmt.Scanf("%c", &key)
 
-	var ptr *int = &i  // 声明一个指针变量，类型为*int，此时ptr的值为&i，指针的数据类型必须与对应值的数据类型一致；指针变量存放的是一个地址，这个地址指向的空间存放的才是值
-	fmt.Println("ptr的值为：", *ptr)   // 指针变量加上*获取内存空间对应的值
-
-	*ptr = 11   // 通过内存地址来修改变量的值
-	fmt.Printf("i的值为：%d, i的内存地址为：%v", i, &i)
-
-	a := 10
-	b := 20
-	a, b = b, a
-	fmt.Printf("a = %d, b = %d", a, b)
+	switch key {   // switch后也可不带表达式，当不带表达式时用case后的表达作为条件进行判断，类似if else分支来使用
+					// 变量声明也可以直接写在switch后面，用分号结束；如 switch key := 10 {
+		case 'a':    // case后各个表达式的数据类型必须和switch后表达式数据类型一致，多个表达式逗号分隔
+			fmt.Println("A")
+		case 'b':
+			fmt.Println("B")
+			fallthrough  // switch穿透，带此关键字不跳出继续执行下一个case
+		case c + 1:
+			fmt.Println("D")
+		default:  // default语句非必须
+			fmt.Println("NO")
+	}
 }
-
