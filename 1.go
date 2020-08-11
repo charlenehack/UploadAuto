a67143c70b0a71cb82c74e0097fb183d31605386
charlenehack <37741776+charlenehack@users.noreply.github.com>
2020-08-11 15:52:02 +0800

    Update 1.go

--- 1.go
+++ 1.go
@@ -1,6 +1,21 @@
-data := url.Values{}
-data.Set("name", "哈罗")
-data.Set("age", "19")
-u, _ := url.ParseRequestURI("http://www.baidu.com")
-u.RawQuery = data.Encode()
-fmt.Println(u.String())
+func main() {
+	url := "http://127.0.0.1:9090/post"
+	// json格式数据
+	contentType := "application/json"
+	data := `{"name": "哈罗", "age": "18"}`
+	// 表单数据
+	//contentType := "application/x-www-form-urlencoded"
+	//data := "name=哈罗&age=18"
+	resp, err := http.Post(url, contentType, strings.NewReader(data))
+	if err != nil {
+		fmt.Printf("post failed, err: %v\n", err)
+		return
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("get response failed, err: %v\n", err)
+		return
+	}
+	fmt.Println(string(body))
+}
