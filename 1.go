5b006d7804c19d420b414752b2d61c36103bc897
charlenehack <37741776+charlenehack@users.noreply.github.com>
2020-08-11 16:18:30 +0800

    Update 1.go

--- 1.go
+++ 1.go
@@ -1,21 +1,13 @@
 func main() {
-	url := "http://127.0.0.1:9090/post"
-	// json格式数据
-	contentType := "application/json"
-	data := `{"name": "哈罗", "age": "18"}`
-	// 表单数据
-	//contentType := "application/x-www-form-urlencoded"
-	//data := "name=哈罗&age=18"
-	resp, err := http.Post(url, contentType, strings.NewReader(data))
+	url := "http://www.baidu.com"
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	req.Header.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36")
+	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Printf("post failed, err: %v\n", err)
-		return
+		//
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Printf("get response failed, err: %v\n", err)
-		return
-	}
 	fmt.Println(string(body))
 }
