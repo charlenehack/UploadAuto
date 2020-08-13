a24f4d600a84177b943525cea6271afef57b1aeb
charlenehack <37741776+charlenehack@users.noreply.github.com>
2020-08-13 16:37:54 +0800

    Update 1.go

--- 1.go
+++ 1.go
@@ -1,38 +1,88 @@
+func main() {
+    f := excelize.NewFile()
+    // 创建一个工作表
+    index := f.NewSheet("Sheet1")
+    // 设置单元格的值
+    f.SetCellValue("Sheet1", "A1", "Hello world.")
+    f.SetCellValue("Sheet1", "B1", 100)
+    // 设置工作簿的默认工作表
+    f.SetActiveSheet(index)
+    // 根据指定路径保存文件
+    if err := f.SaveAs("Book1.xlsx"); err != nil {
+        fmt.Println(err)
+    }
+}
 
-doc,e := goquery.NewDocumentFromReader(reader io.Reader)
-resp, err := http.Get("https://www.baidu.com")
-doc,e := goquery.NewDocumentFromReader(resp.Body)
+func main() {
+    f, err := excelize.OpenFile("Book1.xlsx")
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    // 获取工作表中指定单元格的值
+    cell, err := f.GetCellValue("Sheet1", "B2")
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    // 获取 Sheet1 上所有单元格
+    rows, err := f.GetRows("Sheet1")
+    for _, row := range rows {
+        for _, colCell := range row {
+            fmt.Print(colCell, "\t")
+        }
+        fmt.Println()
+    }
+}
 
+func main() {
+   f, err := excelize.OpenFile("Book1.xlsx")
+   f.SetCellValue("Sheet1", "A2", "wow")
+	f.SetCellValue("Sheet1", "B2", "1407")
+   if err := f.Save(); err != nil {
+       fmt.Println(err)
+    }
+}
 
-doc,e := goquery.NewDocumentFromReader(strings.NewReader(html))
-doc,e := goquery.NewDocument(url string)
-doc,e := goquery.NewDocument("https://www.baidu.com")
-2、查找内容
+import (
+    "fmt"
+    _ "image/gif"
+    _ "image/jpeg"
+    _ "image/png"
 
-ele.Find("#title") //根据id查找
-ele.Find(".title") //根据class查找
-ele.Find("h2").Find("a") //链式调用
-3、获取内容
+    "github.com/360EntSecGroup-Skylar/excelize"
+)
 
-ele.Html()
-ele.Text()
-4、获取属性
-
-ele.Attr("href")
-ele.AttrOr("href", "")
-5、遍历
-
-ele.Find(".item").Each(func(index int, ele *goquery.Selection){
+func main() {
+    f, err := excelize.OpenFile("Book1.xlsx")
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    // 插入图片
+    if err := f.AddPicture("Sheet1", "A2", "image.png", ""); err != nil {
+        fmt.Println(err)
+    }
+    // 在工作表中插入图片，并设置图片的缩放比例
+    if err := f.AddPicture("Sheet1", "D2", "image.jpg", `{
+        "x_scale": 0.5,
+        "y_scale": 0.5
+    }`); err != nil {
+        fmt.Println(err)
+    }
+    // 在工作表中插入图片，并设置图片的打印属性
+    if err := f.AddPicture("Sheet1", "H2", "image.gif", `{
+        "x_offset": 15,
+        "y_offset": 10,
+        "print_obj": true,
+        "lock_aspect_ratio": false,
+        "locked": false
+    }`); err != nil {
+        fmt.Println(err)
+    }
+    // 保存文件
+    if err = f.Save(); err != nil {
+        fmt.Println(err)
+    }
+}
    
-})
-
-doc.Find("span.bio").Text()
-doc.Find(".bio").Text()
-doc.Find("#edit").Text()
-doc.Find("[data-hook=review-title]").Text()
-如果一个选择器对应多个结果，可以使用 First(), Last(), Eq(index int), Slice(start, end int)这些方法进一步定位。
-
-doc.Find("div.profile-navbar").Find("span.num").Eq(1)
-
-doc.Find("div.profile-navbar").Find("a.item").Eq(1).Attr("href")
-doc.Find("div.profile-navbar").Find("a.item").Eq(1).AttrOr("href", "")
