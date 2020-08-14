a5c2b16cb6fbf89c204ec584040286ca9c5cdbfe
charlenehack <37741776+charlenehack@users.noreply.github.com>
2020-08-14 18:02:47 +0800

    Update 1.go

--- 1.go
+++ 1.go
@@ -1,13 +1,16 @@
 func main() {
-	cfg, err := goconfig.LoadConfigFile("conf.ini")
-	if err != nil {
-		panic(err)
-	}
+	match, _ = regexp.MatchString("H(.*)d!", "Hello, world!")  // 返回true或false
+	match, _ = regexp.Match("H(.*)d!", []byte("Hello, world!"))    // 返回true或false
+	
+	r, _ := regexp.Compile("H(.*)d!")
+	reg := regexp.MustCompile("H(.*)d!")  // 与Compile区别在于少一个返回值err
 
-	v, err := cfg.GetValue("default", "test")
-	v1, err := cfg.Int("default", "test")
-	s, err := cfg.GetSection("default")
-	fmt.Printf("%T, %v\n", v, v)
-	fmt.Printf("%T, %v\n", v1, v1)
-	fmt.Printf("%T, %v", s, s)
+
+	match = reg.MatchString("Hello, world!")  // 返回true或false
+	fmt.Println(reg.FindString("Hello, world! haha"))  // 返回第一个匹配到的子串， Hello, world！
+	fmt.Println(reg.FindAllString("Hello, world! haha", -1))  // 返回所有匹配到的子串，n表示匹配数量，[Hello, world!]
+	fmt.Println(reg.FindStringIndex("Hello, world! haha"))  // 返回匹配到子串的起止索引， [0 13]
+	fmt.Println(reg.FindStringSubmatch("Hello, world! haha"))  // 返回第一个匹配到的子串"H(.*)d!"和局部子串"(.*)"， [Hello, world! ello, worl]
+	fmt.Println(reg.FindAllStringSubmatch("Hello, world! haha", -1)) // 返回所有匹配到的子串和局部子串， [[Hello, world! ello, worl]]
+	fmt.Println(reg.ReplaceAllString("Hello world! haha", "html"))  // 将匹配到的结果替换，html haha
 }
