09483c49a1102ac43869732756d8145007778fc8
charlenehack <37741776+charlenehack@users.noreply.github.com>
2020-08-27 14:52:43 +0800

    Update 1.go

--- 1.go
+++ 1.go
@@ -1,4 +1,25 @@
-layout :=  "2006-01-02 15:04:05"
-times, _ := time.Parse(timeLayout, "2014-06-15 08:37:18")  
-timeUnix := times.Unix()  
-fmt.Printf("times is %+v \n, timeUnix is %+v", times, timeUnix)
+1. 删除单个元素
+func delItem(vs []string, s string) []string{
+    for i := 0; i < len(vs); i++ {
+          if s == vs[i] {
+                vs = append(vs[:i], vs[i+1:]...)
+                  i = i-1
+          }
+    }
+    return vs
+}
+2.删除多个元素
+func delItems(vs []string, dels []string) []string {
+      dMap := make(map[string]bool)
+      for _, s := range dels {
+            dMap[s] = true
+      }
+
+      for i := 0; i < len(vs); i++ {
+            if _, ok := dMap[vs[i]]; ok {
+                  vs = append(vs[:i], vs[i+1:]...)
+                  i = i-1
+            }
+      }
+      return vs
+}
