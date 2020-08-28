9266ba32e8b0c118b3b3af15d774921716458c54
charlenehack <37741776+charlenehack@users.noreply.github.com>
2020-08-28 15:24:18 +0800

    Update 1.go

--- 1.go
+++ 1.go
@@ -1,20 +1,20 @@
-dialer, err := proxy.SOCKS5("tcp", "127.0.0.1:8080",
-    &proxy.Auth{User:"username", Password:"password"},
-    &net.Dialer {
-        Timeout: 30 * time.Second,
-        KeepAlive: 30 * time.Second,
+urli := url.URL{}
+urlproxy, _ := urli.Parse("https://127.0.0.1:8080")
+client := http.Client{
+    Transport: &http.Transport{
+        Proxy: http.ProxyURL(urlproxy),  // 进行类型转换
+        Dial: (&net.Dialer{Timeout: 10 * time.Second,}).Dial,   // 不配置超时时间无需
     },
-)
-dialer, err := proxy.SOCKS5("tcp", "127.0.0.1：8080", nil, proxy.Direct)
-
-transport := &http.Transport{
-    Proxy: nil,
-    Dial: dialer.Dial,
-    TLSHandshakeTimeout: 10 * time.Second,
 }
-client := &http.Client { Transport: transport }
-response, err := client.Get("http://mengqi.info")
+resp, err := client.Get("http://myip.top")
 
 req, err := http.NewRequest("GET", "http://myip.top", nil)
 req.Header.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36")
 resp, err := client.Do(req)
+if err != nil {
+	fmt.Printf("get failed, err: %v\n", err)
+	return
+}
+defer resp.Body.Close()
+body, err := ioutil.ReadAll(resp.Body)
+fmt.Println(string(body))
